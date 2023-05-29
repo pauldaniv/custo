75b42e7d98ed801b411eb1723bda69ca33de9478
Pavlo <paulsegfault@gmail.com>
2023-05-29 14:18:39 +0300

    Add .gitconfig population

--- mac-m1-setup.sh
+++ mac-m1-setup.sh
@@ -16,3 +16,35 @@ sdk install gradle
 
 #todo: sdkman, gradle, pyenv, .ssh/config setup
 source ~/.zshrc
+
+echo "Populating .gitconfig-personal"
+tee -a .gitconfig-personal > /dev/null <<EOF
+[user]
+        signingkey = B286731F71468F7BCF4E80F300B9131832CF5F5A
+        name = Pavlo
+        email = paulsegfault@gmail.com
+EOF
+
+tee -a .gitconfig-work > /dev/null <<EOF
+[user]
+        signingkey = 2FB3A344CECD9DC353BCB55A245C609B300925CF
+        name = Pavlo Daniv
+        email = pavlo.daniv@sombrainc.com
+EOF
+
+echo "Populating .gitconfig"
+tee -a .gitconfig > /dev/null <<EOF
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+        ignorecase = true
+        precomposeunicode = true
+[remote "origin"]
+        url = git@github.com:pauldaniv/fresh-setup
+        fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+        remote = origin
+        merge = refs/heads/master
+EOF
