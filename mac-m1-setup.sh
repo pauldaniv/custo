dab9ec545430194651631cef67578725884ba1a3
Pavlo <paulsegfault@gmail.com>
2023-05-29 13:40:38 +0300

    Update mac-m1-setup.sh

--- mac-m1-setup.sh
+++ mac-m1-setup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/pavlodaniv/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 #brew required
 brew install \
 jq \
 kubectl \
-act
+act \
+pyenv
+
+curl -s "https://get.sdkman.io" | bash
+sdk install gradle
 
 #todo: sdkman, gradle, pyenv, .ssh/config setup
 source ~/.zshrc
