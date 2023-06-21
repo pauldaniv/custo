169ec4347e7b332c317d44eebf2f5f4df9da87a0
Pavlo <paulsegfault@gmail.com>
2023-06-21 20:33:54 +0300

    Add alias extension

---
+++ home/.config/custom/extensions/alias.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+alias watch="watch "
+alias k="kubectl"
+alias grsh="git reset --soft HEAD^"
--- home/.config/custom/extensions/python.sh
+++ home/.config/custom/extensions/python.sh
@@ -5,5 +5,15 @@ NC='\033[0m'
 
 if [[ -f "./venv/bin/activate" && -f $auto_venv_config && $(cat $auto_venv_config) == "yes" ]]; then
   source ./venv/bin/activate
-  echo "Activated venv: ${CYAN}`which python`${NC}"
+  echo "Activated venv: ${CYAN}$(which python)${NC}"
 fi
+
+pyctivate() {
+  if [[ "$(cat $auto_venv_config)" = "yes" ]]; then
+    echo "no" >$auto_venv_config
+  elif [[ "$(cat $auto_venv_config)" = "no" ]]; then
+    echo "yes" >$auto_venv_config
+  else
+    echo "no" >$auto_venv_config
+  fi
+}
