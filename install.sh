386d2d7d2424fcac8873a1891e7b89bcffa8e5f2
Pavlo <paulsegfault@gmail.com>
2023-06-22 23:33:52 +0300

    Fix base path

--- install.sh
+++ install.sh
@@ -2,23 +2,19 @@
 
 REPO_NAME="fresh-setup"
 
-mkdir -p ~/.custo
-
-cd ~/.custo
 if [[ -d $REPO_NAME ]]; then
-  cd $REPO_NAME
+  cd .custo
   git pull
 else
   git clone git@github.com:pauldaniv/$REPO_NAME.git
-  cd $REPO_NAME
+  mv $REPO_NAME .custo
+  cd .custo
 fi
 
-cp -r home/ $HOME
-
-if ! grep -q "config/custo/env" "$HOME/.zshrc"; then
+if ! grep -q ".custo/env" "$HOME/.zshrc"; then
   tee -a $HOME/.zshrc <<EOF
 #CUSTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-source ~/.config/custo/env.sh
+source ~/.custo/env.sh
 #END CUSTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 EOF
 fi
