029468ac2e3cea22ba90e4cf6b2864a2c8495ed1
Pavlo <paulsegfault@gmail.com>
2023-06-22 22:35:20 +0300

    Update install file

--- install.sh
+++ install.sh
@@ -9,14 +9,17 @@ cd $REPO_NAME
 
 cp -r home/ $HOME
 
-if ! grep -q "config/custo/env" "~/.zshrc"; then
-  tee -a ~/.zshrc <<EOF
-#CUSTOM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if ! grep -q "config/custo/env" "$HOME/.zshrc"; then
+  tee -a $HOME/.zshrc <<EOF
+#CUSTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 extensions=(
         python
         alias
 )
 source $HOME/.config/custo/env.sh
-#END CUSTOM $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#END CUSTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 EOF
 fi
+
+sed -i '/*.config/custo/env.sh*/$HOME/.config/custo/env.sh/' ~/.zshrc
