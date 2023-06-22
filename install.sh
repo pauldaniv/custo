b077814b236af8eb73f69d0b9a4973df218146d4
Pavlo <paulsegfault@gmail.com>
2023-06-22 23:39:12 +0300

    Update repo cloning

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 
-REPO_NAME="fresh-setup"
-
-if [[ -d $REPO_NAME ]]; then
-  cd .custo
+if [[ -d ~/.custo ]]; then
+  cd ~/.custo
   git pull
 else
-  git clone git@github.com:pauldaniv/$REPO_NAME.git
-  mv $REPO_NAME .custo
-  cd .custo
+  git -C ~ clone git@github.com:pauldaniv/$REPO_NAME.git .custo
+  cd ~/.custo
 fi
 
 if ! grep -q ".custo/env" "$HOME/.zshrc"; then
