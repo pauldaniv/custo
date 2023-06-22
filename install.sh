5341f8284b047778038fdc532753ecfbb0497413
Pavlo <paulsegfault@gmail.com>
2023-06-23 00:16:27 +0300

    Update init script

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 
-if [[ -d ~/.custo ]]; then
+CUSTO_HOME=".custo"
+
+if [[ -d ~/$CUSTO_HOME ]]; then
   cd ~/.custo
   git pull
 else
-  git -C ~ clone https://github.com/pauldaniv/custo.git .custo
+  git -C ~ clone https://github.com/pauldaniv/custo.git $CUSTO_HOME
   cd ~/.custo
 fi
 
-if ! grep -q ".custo/bin/env" "$HOME/.zshrc"; then
+if ! grep -q "$CUSTO_HOME/bin/env" "$HOME/.zshrc"; then
   tee -a $HOME/.zshrc <<EOF
 #CUSTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-source ~/.custo/bin/env.sh
+# Add extensions bellow
+extensions=(
+        python
+        alias
+)
+source ~/$CUSTO_HOME/bin/env.sh
 #END CUSTO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 EOF
 fi
