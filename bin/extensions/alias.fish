77b2704322ba5cf66d7b8dfbe8f255c89f9bb0b9
Pavlo <paulsegfault@gmail.com>
2023-06-23 22:56:43 +0300

    Add aliases

--- bin/extensions/alias.fish
+++ bin/extensions/alias.fish
@@ -3,6 +3,13 @@
 alias watch="watch "
 alias k="kubectl"
 alias grsh="git reset --soft HEAD^"
+alias gpf='git push --force-with-lease --force-if-includes'
+alias l='ls -lah'
+alias la='ls -lAh'
+alias ll='ls -lh'
+alias ls='ls -G'
+alias lsa='ls -lah'
+alias md='mkdir -p'
 
 function nvenv
   set name $argv[1]
