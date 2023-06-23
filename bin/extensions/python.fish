58a4b00fe8bf9b51c7d170bdf342f073bb1b7469
Pavlo <paulsegfault@gmail.com>
2023-06-23 22:18:02 +0300

    Remove colors from python.fish

--- bin/extensions/python.fish
+++ bin/extensions/python.fish
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-CYAN='\033[1;36m'
-NC='\033[0m'
-
 if test -e "./venv/bin/activate" && test -e $auto_venv_config && $(cat $auto_venv_config) == "yes"
   source ./venv/bin/activate
   echo "Activated venv: ${CYAN}$(which python)${NC}"
