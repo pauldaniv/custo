a097b26c3ed166bf6bb8e168475f7908e6f71c55
Pavlo <paulsegfault@gmail.com>
2023-06-24 19:44:26 +0300

    Fix version message for fish

--- bin/common/commons.fish
+++ bin/common/commons.fish
@@ -83,7 +83,7 @@ function custo
     ~/.custo/install.sh
     source ~/.config/fish/config.fish
   else if test $command = "version"
-    echo -e "$(git -C ~/.custo --no-pager branch) $Green->$NC $(git -C ~/.custo rev-parse --short HEAD) $Blue->$NC $(git -C ~/.custo --no-pager show -s --format=%s)"
+    echo -e "$(git -C ~/.custo --no-pager branch --show-current) $Green->$NC $(git -C ~/.custo rev-parse --short HEAD) $Blue->$NC $(git -C ~/.custo --no-pager show -s --format=%s)"
   else
     echo "Unsupported command"
   end
