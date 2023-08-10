bf71e1ddd348c7cd4bd4613d212c2286611cc63f
Pavlo <paulsegfault@gmail.com>
2023-08-10 18:45:52 +0300

    Fix activenv function order

--- bin/extensions/python.fish
+++ bin/extensions/python.fish
@@ -2,13 +2,13 @@
 
 set auto_venv_config $HOME/.config/custom/auto_venv.txt
 
-if test -e "./venv/bin/activate" && test -e $auto_venv_config && test (cat $auto_venv_config) = "yes"
-  activenv
-end
-
 function activenv
   source ./venv/bin/activate.fish
   echo -e "Activated venv: $CYAN$(which python)$NC"
+end
+
+if test -e "./venv/bin/activate" && test -e $auto_venv_config && test (cat $auto_venv_config) = "yes"
+  activenv
 end
 
 function auto_venv
