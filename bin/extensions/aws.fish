4ea12a32021d54220675a9c9787011e65fccc77b
Pavlo <paulsegfault@gmail.com>
2023-12-07 23:43:24 +0200

    Add aws_sso_to_env function for fish

---
+++ bin/extensions/aws.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function aws_sso_to_env
+   cache_file=(ls -Art ~/.aws/cli/cache/ | tail -n 1)
+
+   set -x AWS_ACCESS_KEY_ID (cat $cache_file | jq -r '.Credentials.AccessKeyId')
+   set -x AWS_SECRET_ACCESS_KEY (cat $cache_file | jq -r '.Credentials.SecretAccessKey')
+   set -x AWS_SESSION_TOKEN (cat $cache_file | jq -r '.Credentials.SessionToken')
+end
+
