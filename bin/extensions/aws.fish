6061097eef7d9372d17d6327b89f2ec80cd6504e
Pavlo <paulsegfault@gmail.com>
2024-10-16 14:50:14 +0300

    Convert aws helper to fish

--- bin/extensions/aws.fish
+++ bin/extensions/aws.fish
@@ -20,41 +20,42 @@ function aws_config_set
     set -l aws_session_token=""
 
     # Read lines from stdin
-    while IFS= read -r line; do
+    while read -l line
         # Break the loop on an empty line
-        if [[ -z "$line" ]]; then
+        if test -z "$line"
             break
-        fi
+        end
 
         # Skip lines starting with '<s-' as they are meant to be ignored
-        if [[ "$line" =~ ^\<s- ]]; then
+        if string match -r '^<s-' "$line"
             continue
-        fi
+        end
 
-        # Detect AWS Access Key ID
-        if [[ "$line" =~ ^aws_access_key_id=.*$ ]]; then
-            set -l aws_access_key_id (echo "$line" | cut -d '=' -f 2)
-            continue
-        fi
+       # Detect AWS Access Key ID
+       if string match -r '^aws_access_key_id=.*$' "$line"
+           set -l aws_access_key_id (echo "$line" | cut -d '=' -f 2)
+           continue
+       end
 
-        # Detect AWS Secret Access Key
-        if [[ "$line" =~ ^aws_secret_access_key=.*$ ]]; then
-            set -l aws_secret_access_key (echo "$line" | cut -d '=' -f 2)
-            continue
-        fi
+       # Detect AWS Secret Access Key
+       if string match -r '^aws_secret_access_key=.*$' "$line"
+           set -l aws_secret_access_key (echo "$line" | cut -d '=' -f 2)
+           continue
+       end
 
-        # Detect AWS Session Token
-        if [[ "$line" =~ ^aws_session_token=.*$ ]]; then
-            set -l aws_session_token (echo "$line" | cut -d '=' -f 2)
-            continue
-        fi
-    done
+       # Detect AWS Session Token
+       if string match -r '^aws_session_token=.*$' "$line"
+           set -l aws_session_token (echo "$line" | cut -d '=' -f 2)
+           continue
+       end
+    end
 
     # Validate that required information is gathered
-    if [[ -z "$profile_name" || -z "$aws_access_key_id" || -z "$aws_secret_access_key" ]]; then
+    if test -z "$profile_name"; or test -z "$aws_access_key_id"; or test -z "$aws_secret_access_key"
         echo "Error: Missing required AWS credentials."
         exit 1
-    fi
+    end
+
 
     # Set the AWS credentials using the AWS CLI
     aws configure set aws_access_key_id "$aws_access_key_id" --profile "$profile_name"
